6cba6ffc195909137582a915b83e33ee0373aaa1
6135 <guibscosta@hotmail.com>
2020-10-06 18:29:44 +0100

    fixed them junits

--- src/test/GreetingTests.java
+++ src/test/GreetingTests.java
@@ -1,14 +1,11 @@
 
 package test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
 import org.junit.Test;
 import app.*;
 public class GreetingTests {
     @Test
     public void testGreeting() {
         assertEquals("hello!", new Greeting().getGreeting());
-        assertNotEquals("Hello!", new Greeting().getGreeting());
     }
 }
